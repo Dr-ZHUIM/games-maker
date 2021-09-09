c61103c28a549f73522668a0f86812631c7a96ae
37291 <372919916@qq.com>
2021-09-09 21:10:46 +0800

    update2021/9/9

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react'
-import {Button,message} from 'antd'
+import { HashRouter, Switch, Route } from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+import Register from './pages/register/register'
+import Forgot from './pages/forgot/forgot'
 //应用根组件
 
 export default class App extends Component {
 
-    handleClick = () => {
-        message.success('success!')
-    }
-
 
     render() {
         return (
-            <Button type='primary' onClick={this.handleClick}>Test</Button>
+            <HashRouter>
+                <Switch>
+                    {/* 路由 key对应value       前台路由value对应组件component   后台路由value对应回调函数 */}
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/forgot" component={Forgot} /> 
+                    <Route path="/" component={Admin} />
+                </Switch>
+            </HashRouter>
         )
     }
 
